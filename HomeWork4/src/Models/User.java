db4877cbdc6f577c1d9c4c2b71db3210a8f65999
Maksim Dl <maksimdolgov@mail.ru>
2023-10-05 17:46:30 +0700

    add sem4 homework for architeck PO

---
+++ HomeWork4/src/Models/User.java
@@ -0,0 +1,69 @@
+package Models;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Модель пользователя
+ */
+public class User {
+    private int id;
+    private String userName;
+    private int hashPassword;
+    private int phone;
+    private List<String> benefits;
+    private long cardNumber;
+
+    public User(int id, String userName, int hashPassword,  long cardNumber, int phone) {
+        this.id = id;
+        this.userName = userName;
+        this.hashPassword = hashPassword;
+        this.cardNumber = cardNumber;
+        this.phone = phone;
+        this.benefits = new ArrayList<>();
+
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getUserName(){
+        return userName;
+    }
+
+    public int getHashPassword(){
+        return hashPassword;
+    }
+
+    public long getCardNumber(){
+        return cardNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Client { " +
+                "id= " + id +
+                ", userName= " + userName +
+                ", cardNumber= " + (String.format("%016d", cardNumber)) +
+                " }";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || this.getClass() != o.getClass()) return false;
+        User client = (User) o;
+        return this.equals(client);
+    }
+
+
+    public boolean equals(User client) {
+        return id == client.id && hashPassword == client.hashPassword && cardNumber == client.cardNumber && userName.equals(client.userName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, userName, hashPassword, cardNumber);
+    }
+}
